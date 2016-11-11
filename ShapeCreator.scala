bf6e11e3563882f4d2a3b6fcef4099f636777b70
Nicholas Cobb <ncobb314@cs.utexas.edu>
2016-11-11 17:21:38 -0600

    yeet2 We did circle, rect, square

--- ShapeCreator.scala
+++ ShapeCreator.scala
@@ -8,66 +8,111 @@ class ShapeCreator {
   
 }
 
-class Circle {
+abstract class Shape {
   var name: String = "";
-  var width: Int = 0;
-  var height: Int = 0;
   var x: Int = 0;
   var y: Int = 0;
   var color: Color = new Color(0, 0, 0);
   var fill: Boolean = true;
-  
-  def AT(x: Int, y: Int): Circle = {
+
+  def AT(x: Int, y: Int): this.type = {
     this.x = x;
     this.y = y;
     return this;
   }
   
-  def SETX(x: Int): Circle = {
+  def SETX(x: Int): this.type = {
     this.x = x;
     return this;
   }
   
-  def SETY(y: Int): Circle = {
+  def SETY(y: Int): this.type = {
     this.y = y;
     return this;
   }
   
-  def DIMENSIONS(height: Int, width: Int): Circle = {
-    this.width = width;
-    this.height = height;
-    return this;
-  }
-  
-  def WIDTH(width: Int): Circle = {
-    this.width = width;
-    return this;
-  }
-  
-  def HEIGHT(height: Int): Circle = {
-    this.height = height;
-    return this;
-  }
-  
-  def COLOR(r: Int, g: Int, b: Int): Circle = {
+  def COLOR(r: Int, g: Int, b: Int): this.type = {
     this.color = new Color(r, g, b);
     return this;
   }
   
-  def COLOR(color: String): Circle = {
+  def COLOR(color: String): this.type = {
     // TODO: string pattern matching?
     return this;
   }
   
-  def FILLSTATE(state: String): Circle = {
+  def FILLSTATE(state: String): this.type = {
     this.fill = "outline".equals(state);
     return this;
+  }
+  
+  def draw(graphics: Graphics)
+  
+}
+
+class Circle extends Shape {
+  var radius: Int = 0;
+  
+  def RADIUS(radius: Int): this.type = {
+    this.radius = radius;
+    return this;
+  }
+  
+  override def draw(graphics: Graphics) {
+    graphics.setColor(this.color);
+    if (this.fill) {
+      graphics.fillOval(this.x, this.y, radius * 2, radius * 2);
+    } else {
+      graphics.drawOval(this.x, this.y, radius * 2, radius * 2);
+    }
+  }
+}
+
+class Rectangle extends Shape {
+  var width: Int = 0;
+  var height: Int = 0;
+  
+  def WIDTH(width: Int): this.type = {
+    this.width = width;
+    return this;
+  }
+  
+  def HEIGHT(height: Int): this.type = {
+    this.height = height;
+    return this;
+  }
+  
+  override def draw(graphics: Graphics) {
+    graphics.setColor(this.color);
+    if (this.fill) {
+      graphics.fillRect(this.x, this.y, this.width, this.height);
+    } else {
+      graphics.drawRect(this.x, this.y, this.width, this.height);
+    }
+  }
+}
+
+class Square extends Shape {
+  var side: Int = 0;
+  
+  def SIDE(side: Int): this.type = {
+    this.side = side;
+    return this;
+  }
+  
+  override def draw(graphics: Graphics) {
+    graphics.setColor(this.color);
+    if (this.fill) {
+      graphics.fillRect(this.x, this.y, this.side, this.side);
+    } else {
+      graphics.drawRect(this.x, this.y, this.side, this.side);
+    }
   }
 }
 
 class CommandRead {
   var panelMap: Map[String, DrawingPanel] = new HashMap[String, DrawingPanel]();
-  var shapeMap: Map[String, Circle] = new HashMap[String, Circle]();
+  var shapeMap: Map[String, Shape] = new HashMap[String, Shape]();
   
   object NEWCIRCLE {
     def WITHNAME(parameterName: String): Circle = {
@@ -78,17 +123,45 @@ class CommandRead {
     }
   }
   
-  object UPDATECIRCLE {
-    def apply(circleName: String): Circle = {
-      return shapeMap.get(circleName);
+  object UPDATE {
+    def CIRCLE(circleName: String): Circle = {
+      return shapeMap.get(circleName).asInstanceOf[Circle];
+    }
+    
+    def RECT(circleName: String): Rectangle = {
+      return shapeMap.get(circleName).asInstanceOf[Rectangle];
+    }
+    
+    
+    def SQUARE(circleName: String): Square = {
+      return shapeMap.get(circleName).asInstanceOf[Square];
+    }
+  }
+  
+  object NEWRECT {
+    def WITHNAME(parameterName: String): Rectangle = {
+      var newRect: Rectangle = new Rectangle();
+      newRect.name = parameterName;
+      shapeMap.put(parameterName, newRect);
+      return newRect;
+    }
+  }
+  
+  object NEWSQUARE {
+    def WITHNAME(parameterName: String): Square = {
+      var newSquare: Square = new Square();
+      newSquare.name = parameterName;
+      shapeMap.put(parameterName, newSquare);
+      return newSquare;
     }
   }
   
   object DRAWTOPANEL {
     def apply(shapeName: String, panelName: String) {
       if (panelMap.containsKey(panelName) && shapeMap.containsKey(shapeName)) {
-        var circle: Circle = shapeMap.get(shapeName);
-        panelMap.get(panelName).getGraphics.fillOval(circle.x, circle.y, circle.width, circle.height);
+        var shape: Shape = shapeMap.get(shapeName);
+        var graphics: Graphics = panelMap.get(panelName).getGraphics();
+        shape.draw(graphics);
       }
     }
   }
--- ShapeTester.scala
+++ ShapeTester.scala
@@ -1,8 +1,13 @@
 object ShapeTester extends CommandRead {
   def main (args: Array[String]) {
     println("!!!!");
-    NEWCIRCLE WITHNAME "C" AT(0,0) DIMENSIONS (20, 20) COLOR(255, 0,0); 
-    PANEL("P", 640, 488);
+    NEWCIRCLE WITHNAME "C" AT(0,0) RADIUS 10 COLOR(255, 0, 0); 
+    NEWRECT WITHNAME "R" WIDTH 10 HEIGHT 15 AT(40, 10) COLOR(0, 255, 0);
+    NEWSQUARE WITHNAME "S" SIDE 62 AT(62, 20) COLOR(0, 255, 0);
+    UPDATE SQUARE "S" COLOR(0, 0, 255);
+    PANEL("P", 640, 480);
     DRAWTOPANEL("C", "P");
+    DRAWTOPANEL("R", "P");
+    DRAWTOPANEL("S", "P");
   }
 }

53f1406ea269ab55db5210c010c7bee3f2a0c6a6
CornCobb314 <ncobb314@gmail.com>
2016-11-14 20:03:10 -0600

    Add files via upload
    yeet3 Composite

--- ShapeCreator.scala
+++ ShapeCreator.scala
@@ -8,12 +8,10 @@ class ShapeCreator {
   
 }
 
-abstract class Shape {
+abstract class BaseShape {
   var name: String = "";
   var x: Int = 0;
   var y: Int = 0;
-  var color: Color = new Color(0, 0, 0);
-  var fill: Boolean = true;
 
   def AT(x: Int, y: Int): this.type = {
     this.x = x;
@@ -31,6 +29,15 @@ abstract class Shape {
     return this;
   }
   
+  def duplicate(newName: String): BaseShape
+  
+  def draw(graphics: Graphics, xAdj: Int, yAdj: Int)
+}
+
+abstract class Shape extends BaseShape {
+  var color: Color = new Color(0, 0, 0);
+  var fill: Boolean = true;
+  
   def COLOR(r: Int, g: Int, b: Int): this.type = {
     this.color = new Color(r, g, b);
     return this;
@@ -45,9 +52,6 @@ abstract class Shape {
     this.fill = "outline".equals(state);
     return this;
   }
-  
-  def draw(graphics: Graphics)
-  
 }
 
 class Circle extends Shape {
@@ -58,13 +62,64 @@ class Circle extends Shape {
     return this;
   }
   
-  override def draw(graphics: Graphics) {
+  override def draw(graphics: Graphics, xAdj: Int, yAdj: Int) {
     graphics.setColor(this.color);
     if (this.fill) {
-      graphics.fillOval(this.x, this.y, radius * 2, radius * 2);
+      graphics.fillOval(this.x + xAdj, this.y + yAdj, radius * 2, radius * 2);
     } else {
-      graphics.drawOval(this.x, this.y, radius * 2, radius * 2);
+      graphics.drawOval(this.x + xAdj, this.y + yAdj, radius * 2, radius * 2);
     }
+  }
+  
+  override def duplicate(newName: String): Circle = {
+    var newCircle: Circle = new Circle();
+    
+    newCircle.name = newName;
+    newCircle.x = this.x;
+    newCircle.y = this.y;
+    newCircle.color = this.color;
+    newCircle.fill = this.fill;
+    
+    newCircle.radius = this.radius;
+    return newCircle;
+  }
+}
+
+class Oval extends Shape {
+  var radiusX: Int = 0;
+  var radiusY: Int = 0;
+  
+  def RADIUSX(radiusX: Int): this.type = {
+    this.radiusX = radiusX;
+    return this;
+  }
+  
+  def RADIUSY(radiusY: Int): this.type = {
+    this.radiusY = radiusY;
+    return this;
+  }
+  
+  override def draw(graphics: Graphics, xAdj: Int, yAdj: Int) {
+    graphics.setColor(this.color);
+    if (this.fill) {
+      graphics.fillOval(this.x + xAdj, this.y + yAdj, radiusX * 2, radiusY * 2);
+    } else {
+      graphics.drawOval(this.x + xAdj, this.y + yAdj, radiusX * 2, radiusY * 2);
+    }
+  }
+  
+  override def duplicate(newName: String): Oval = {
+    var newOval: Oval = new Oval();
+    
+    newOval.name = newName;
+    newOval.x = this.x;
+    newOval.y = this.y;
+    newOval.color = this.color;
+    newOval.fill = this.fill;
+    
+    newOval.radiusX = this.radiusX;
+    newOval.radiusY = this.radiusY;
+    return newOval;
   }
 }
 
@@ -82,13 +137,27 @@ class Rectangle extends Shape {
     return this;
   }
   
-  override def draw(graphics: Graphics) {
+  override def draw(graphics: Graphics, xAdj: Int, yAdj: Int) {
     graphics.setColor(this.color);
     if (this.fill) {
-      graphics.fillRect(this.x, this.y, this.width, this.height);
+      graphics.fillRect(this.x + xAdj, this.y + yAdj, this.width, this.height);
     } else {
-      graphics.drawRect(this.x, this.y, this.width, this.height);
+      graphics.drawRect(this.x + xAdj, this.y + yAdj, this.width, this.height);
     }
+  }
+  
+  override def duplicate(newName: String): Rectangle = {
+    var newRect: Rectangle = new Rectangle();
+    
+    newRect.name = newName;
+    newRect.x = this.x;
+    newRect.y = this.y;
+    newRect.color = this.color;
+    newRect.fill = this.fill;
+    
+    newRect.width = this.width;
+    newRect.height = this.height;
+    return newRect;
   }
 }
 
@@ -100,28 +169,134 @@ class Square extends Shape {
     return this;
   }
   
-  override def draw(graphics: Graphics) {
+  override def draw(graphics: Graphics, xAdj: Int, yAdj: Int) {
     graphics.setColor(this.color);
     if (this.fill) {
-      graphics.fillRect(this.x, this.y, this.side, this.side);
+      graphics.fillRect(this.x + xAdj, this.y + yAdj, this.side, this.side);
     } else {
-      graphics.drawRect(this.x, this.y, this.side, this.side);
+      graphics.drawRect(this.x + xAdj, this.y + yAdj, this.side, this.side);
     }
+  }
+  
+  override def duplicate(newName: String): Square = {
+    var newSquare: Square = new Square();
+    
+    newSquare.name = newName;
+    newSquare.x = this.x;
+    newSquare.y = this.y;
+    newSquare.color = this.color;
+    newSquare.fill = this.fill;
+    
+    newSquare.side = this.side;
+    return newSquare;
+  }
+}
+
+class Triangle extends Shape {
+  var base: Int = 0;
+  var height: Int = 0;
+  
+  def BASE(base: Int): this.type = {
+    this.base = base;
+    return this;
+  }
+  
+  def HEIGHT(height: Int): this.type = {
+    this.height = height;
+    return this;
+  }
+  
+  override def draw(graphics: Graphics, xAdj: Int, yAdj: Int) {
+    graphics.setColor(this.color);
+    if (this.fill) {
+		  graphics.fillPolygon(getTriangleX(x, y, height, base, xAdj), getTriangleY(x, y, height, base, yAdj), 3);
+    } else {
+		  graphics.drawPolygon(getTriangleX(x, y, height, base, xAdj), getTriangleY(x, y, height, base, yAdj), 3);
+    }
+  }
+  
+  override def duplicate(newName: String): Triangle = {
+    var newTri: Triangle = new Triangle();
+    
+    newTri.name = newName;
+    newTri.x = this.x;
+    newTri.y = this.y;
+    newTri.color = this.color;
+    newTri.fill = this.fill;
+    
+    newTri.base = this.base;
+    newTri.height = this.height;
+    return newTri;
+  }
+  
+  private def getTriangleX(x: Int, y: Int, height: Int, base: Int, xAdj: Int): Array[Int] = {
+    return Array(x + xAdj, x + base + xAdj, x + (base * 1.0 / 2).toInt + xAdj);
+  }
+  private def getTriangleY(x: Int, y: Int, height: Int, base: Int, yAdj: Int): Array[Int] = {
+    return Array(y + height + yAdj, y + height + yAdj, y + yAdj);
+  }
+}
+
+
+class Composite extends BaseShape {
+  var shapes: ArrayList[BaseShape] = new ArrayList[BaseShape]();
+  var duplicate: Boolean = false;
+   var shapeMap: Map[String, BaseShape] = new HashMap[String, BaseShape]();
+
+  
+  def FROM(shapes: String*): Composite = {
+    var shapesArray: Array[String] = shapes.toArray;
+    var length: Int = shapesArray.length - 1;
+    
+    for (i <- 0 to length) {
+      this.addShape(shapeMap.get(shapesArray(i)), shapesArray(i), this.duplicate);
+    }
+    
+    return this;
+  }
+  
+  def addShape(shape: BaseShape, newName: String, duplicate: Boolean) {
+    if (duplicate) {
+      this.shapes.add(0, shape.duplicate(newName));
+    } else {
+      this.shapes.add(0, shape);
+    }
+  }
+  
+  def removeShape(newName: String) {
+    for (i <- 0 to shapes.size() - 1) {
+      if (shapes.get(i).name.equals(newName)) {
+        shapes.remove(i);
+        return;
+      }
+    }
+  }
+  
+  def draw(graphics: Graphics, xAdj: Int, yAdj: Int) {
+    for (i <- 0 to shapes.size() - 1) {
+      shapes.get(i).draw(graphics, xAdj, yAdj);
+    }
+  }
+  
+  def duplicate(newName: String): BaseShape = {
+    var newComp: Composite = new Composite();
+    
+    newComp.name = newName;
+    newComp.x = this.x;
+    newComp.y = this.y;
+    
+    for (i <- 1 to shapes.size()) {
+      var currShape: BaseShape = shapes.get(shapes.size - i); 
+      newComp.addShape(currShape, currShape.name, true);
+    }
+    
+    return newComp;
   }
 }
 
 class CommandRead {
   var panelMap: Map[String, DrawingPanel] = new HashMap[String, DrawingPanel]();
-  var shapeMap: Map[String, Shape] = new HashMap[String, Shape]();
-  
-  object NEWCIRCLE {
-    def WITHNAME(parameterName: String): Circle = {
-      var newCircle: Circle = new Circle();
-      newCircle.name = parameterName;
-      shapeMap.put(parameterName, newCircle);
-      return newCircle;
-    }
-  }
+  var shapeMap: Map[String, BaseShape] = new HashMap[String, BaseShape]();
   
   object UPDATE {
     def CIRCLE(circleName: String): Circle = {
@@ -132,9 +307,33 @@ class CommandRead {
       return shapeMap.get(circleName).asInstanceOf[Rectangle];
     }
     
-    
     def SQUARE(circleName: String): Square = {
       return shapeMap.get(circleName).asInstanceOf[Square];
+    }
+    
+    def OVAL(ovalName: String): Oval = {
+      return shapeMap.get(ovalName).asInstanceOf[Oval];
+    }
+    
+    def TRI(triName: String): Triangle = {
+      return shapeMap.get(triName).asInstanceOf[Triangle];
+    }
+  }
+  
+  object NEWCIRCLE {
+    def WITHNAME(parameterName: String): Circle = {
+      var newCircle: Circle = new Circle();
+      newCircle.name = parameterName;
+      shapeMap.put(parameterName, newCircle);
+      return newCircle;
+    }
+  }
+  object NEWOVAL {
+    def WITHNAME(parameterName: String): Oval = {
+      var newOval: Oval = new Oval();
+      newOval.name = parameterName;
+      shapeMap.put(parameterName, newOval);
+      return newOval;
     }
   }
   
@@ -156,12 +355,21 @@ class CommandRead {
     }
   }
   
+  object NEWTRI {
+    def WITHNAME(parameterName: String): Triangle = {
+      var newTri: Triangle = new Triangle();
+      newTri.name = parameterName;
+      shapeMap.put(parameterName, newTri);
+      return newTri;
+    }
+  }
+  
   object DRAWTOPANEL {
-    def apply(shapeName: String, panelName: String) {
+    def apply(shapeName: String, panelName: String, x: Int, y: Int) {
       if (panelMap.containsKey(panelName) && shapeMap.containsKey(shapeName)) {
-        var shape: Shape = shapeMap.get(shapeName);
+        var shape: BaseShape = shapeMap.get(shapeName);
         var graphics: Graphics = panelMap.get(panelName).getGraphics();
-        shape.draw(graphics);
+        shape.draw(graphics, x, y);
       }
     }
   }
@@ -170,6 +378,25 @@ class CommandRead {
     def apply(panelName: String, width: Int, height: Int) {
       var newPanel: DrawingPanel = new DrawingPanel(width, height);
       panelMap.put(panelName, newPanel);
+    }
+  }
+  
+  object MAKE {
+    def A(parameterName: String): Composite = {
+      return createComp(parameterName, false);
+    }
+    
+    def DUPLICATE(parameterName: String) : Composite = {
+      return createComp(parameterName, true);
+    }
+    
+    def createComp(parameterName: String, duplicate: Boolean): Composite = {
+      var newComp: Composite = new Composite();
+      newComp.name = parameterName;
+      shapeMap.put(parameterName, newComp);
+      newComp.duplicate = duplicate;
+      newComp.shapeMap = shapeMap;
+      return newComp;
     }
   }
 }
--- ShapeTester.scala
+++ ShapeTester.scala
@@ -2,12 +2,11 @@ object ShapeTester extends CommandRead {
   def main (args: Array[String]) {
     println("!!!!");
     NEWCIRCLE WITHNAME "C" AT(0,0) RADIUS 10 COLOR(255, 0, 0); 
-    NEWRECT WITHNAME "R" WIDTH 10 HEIGHT 15 AT(40, 10) COLOR(0, 255, 0);
-    NEWSQUARE WITHNAME "S" SIDE 62 AT(62, 20) COLOR(0, 255, 0);
-    UPDATE SQUARE "S" COLOR(0, 0, 255);
+    NEWSQUARE WITHNAME "S" SIDE 62 AT(20, 20) COLOR(0, 255, 0);
+    
+    MAKE A "comp" FROM("C", "S");
     PANEL("P", 640, 480);
-    DRAWTOPANEL("C", "P");
-    DRAWTOPANEL("R", "P");
-    DRAWTOPANEL("S", "P");
+    
+    DRAWTOPANEL("comp", "P", 0, 0);
   }
 }

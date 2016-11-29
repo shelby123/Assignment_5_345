ef62ca7e34944e2873f0df856078697843abe39f
shelby123 <shelbymcgarrah@windstream.net>
2016-11-29 07:53:15 -0600

    changed syntax/ pattern fix

--- Rain.scala
+++ Rain.scala
@@ -6,22 +6,27 @@ object Rain extends CommandRead {
     NEWCIRCLE WITHNAME "C" RADIUS 10 COLOR "LIGHT BLUE" AT (0, 22);
     
     MAKE A "RAINDROP" FROM("TOP", "C") SCALE .5 
+    MAKE DUPLICATE "RAINDROP2" FROM ("TOP", "C") SCALE .75 AT ((640/30), 0)
+      
+    GET TRI "TOP" COLOR "OCEAN BLUE"
+    GET CIRCLE "C" COLOR "OCEAN BLUE"
 
     GETVAR ("I");
     SETVAR ("I", 0)
     PATTERN("I", 10, 1) {
         GET COMPOSITE "RAINDROP" SETX 0
+        GET COMPOSITE "RAINDROP2" SETX (640/30)
         GETVAR ("J")
         SETVAR ("J", 0) 
         PATTERN("J", 15, 1) {
-          DRAWTOPANEL("RAINDROP", "P", 0, 0)
-          ((GET COMPOSITE "RAINDROP") * 1.5 ) >> (640/30)
-          DRAWTOPANEL("RAINDROP", "P", 0, 0)
-          (GET COMPOSITE "RAINDROP") / 1.5
-          (GET COMPOSITE "RAINDROP") >> (640/30)
-        }
-    }
-      (GET COMPOSITE "RAINDROP") vv (480/10)
+           DRAWTOPANEL("RAINDROP", "P", 0, 0)
+           (GET COMPOSITE "RAINDROP")  >> (640/15)
+           DRAWTOPANEL("RAINDROP2", "P", 0, 0)
+           (GET COMPOSITE "RAINDROP2") >> (640/15)
 
+        }
+        (GET COMPOSITE "RAINDROP") vv (480/10)
+        (GET COMPOSITE "RAINDROP2") vv (480/10)
+    }
   }
 }
